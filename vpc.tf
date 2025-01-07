bbc4e2bb2aabb180b2c1045fbe50b85e43db823f
vasanthi111 <125528976+vasanthi111@users.noreply.github.com>
2025-01-07 11:12:23 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = var.region
+}
+resource "aws_vpc" "demovpc" {
+cidr_block="10.0.0.0/16"
+instance_tenancy="default"
+tags={
+Name="DEMO VPC"
+}
+}
+
+resource "aws_subnet" "public_subnet-1" {
+vpc_id="${aws_vpc.demovpc.id}"
+cidr_block="10.0.1.0/24"
+map_public_ip_on_launch=true
+availability_zone="us-east-1a"
+tags={
+Name="Web Subnet 1"
+}
+}
